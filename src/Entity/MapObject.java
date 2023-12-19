998e7ff5d68eac6ef15aef7e5a4a4bce9bd07837
fiodo <aliusfiodorovas@gmail.com>
2023-12-19 14:25:38 +0200

    l

--- src/Entity/MapObject.java
+++ src/Entity/MapObject.java
@@ -1,6 +1,8 @@
 package Entity;
 
+import Main.GamePanel;
 import TileMap.TileMap;
+import TileMap.Tile;
 
 import java.awt.*;
 
@@ -70,10 +72,120 @@ public abstract class MapObject {
     }
 
     public boolean intersects (MapObject o) {
-        Rectangle r1 = new Rectangle()
+        Rectangle r1 = getRectangle();
+        Rectangle r2 = o.getRectangle();
+        return r1.intersects(r2);
     }
 
+    public Rectangle getRectangle(){
+        return new Rectangle((int)x - cwidth,(int)y -cheight, cwidth,cheight);
+    }
 
+    public void checkTileMapCollision() {
+        currCol = (int)x / tileSize;
+        currRow = (int)y / tileSize;
+
+        xdest = x + dx;
+        ydest = y + dy;
+        xtemp = x;
+        ytemp = y;
+
+        calculateCorners(x, ydest);
+        if (dy < 0) {
+            if(topLeft || topRight){
+                dy = 0;
+                ytemp = currRow * tileSize + cheight / 2;
+            }
+            else{
+                ytemp += dy;
+            }
+        }
+        if (dy > 0) {
+            if (bottomLeft || bottomRight) {
+                dy = 0;
+                falling = false;
+                ytemp = (currRow + 1) * tileSize - cheight / 2;
+            }
+            else {
+                ytemp += dy;
+            }
+        }
+
+        calculateCorners(xdest,y);
+
+        if (dx < 0) {
+            if (topLeft || bottomLeft) {
+                dx = 0;
+                xtemp = currCol * tileSize + cwidth / 2;
+            }
+        }
+        else {
+            xtemp += dx;
+        }
+        if (dx > 0) {
+            if (topRight || bottomRight) {
+                dx = 0;
+                xtemp = (currCol +1) * tileSize - cwidth / 2;
+            }
+        }
+        if (!falling) {
+            calculateCorners(x,xdest + 1);
+            if (!bottomLeft && !bottomRight) {
+                falling = true;
+            }
+        }
+    }
+
+    public int getx() {return (int) x;}
+    public int gety() {return (int) y;}
+    public int getWidth() {return width;}
+    public int getHeight() {return height;}
+    public int getCWidth() {return cwidth;}
+    public int getCHeight() {return cheight;}
+
+    public void setPosition (double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setVector (double dx, double dy) {
+        this.dx = dx;
+        this.dy = dy;
+    }
+
+    public void setMapPosition() {
+        xmap = tileMap.getx();
+        ymap = tileMap.gety();
+    }
+
+    public void setLeft (boolean b) { left = b;}
+    public void setRight (boolean b) {right = b;}
+    public void setUp (boolean b) { up = b;}
+    public void setDown (boolean b) {down = b;}
+    public void setJumping (boolean b) {jumping = b;}
+
+    public boolean notOnScreen () {
+        return x + xmap + width < 0 || x + xmap - width > GamePanel.WIDTH || y + ymap + height < 0 || y + ymap - height > GamePanel.HEIGHT;
+    }
+
+    private void calculateCorners(double x, double ydest) {
+
+        int leftTile = (int) (x - cwidth / 2) / tileSize;
+        int rightTile = (int) (x + cwidth / 2 - 1) / tileSize;
+        int topTile = (int) (y - cheight / 2) / tileSize;
+        int botTile = (int) (y + cheight / 2 - 1) / tileSize;
+
+        int tl = tileMap.getType(topTile,leftTile);
+        int tr = tileMap.getType(topTile,rightTile);
+        int bl = tileMap.getType(botTile,leftTile);
+        int br = tileMap.getType(botTile,rightTile);
+
+        topLeft = tl == Tile.BLOCKED;
+        topRight = tr == Tile.BLOCKED;
+        bottomLeft = bl == Tile.BLOCKED;
+        bottomRight = br == Tile.BLOCKED;
+
+    }
 
 
 }
