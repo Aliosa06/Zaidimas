7f9bbac2a4429bbe6b418318e824f305b4d02fc9
Labas <aliusfiodorovas@gmail.com>
2023-12-19 01:08:11 +0100

    Idetas mapas, tiles

--- src/GameState/Level1State.java
+++ src/GameState/Level1State.java
@@ -2,9 +2,14 @@ package GameState;
 
 import java.awt.*;
 
+import Main.GamePanel;
+import TileMap.*;
+
+
 public class Level1State extends GameState{
 
     private TileMap tileMap;
+    private Background bg;
 
     public Level1State (GameStateManager gsm) {
         this.gsm = gsm;
@@ -13,6 +18,13 @@ public class Level1State extends GameState{
 
     @Override
     public void init() {
+
+        tileMap = new TileMap(30);
+        tileMap.loadTiles("/Tilesets/grasstileset.gif");
+        tileMap.loadMap("/Maps/level1-1.map");
+        tileMap.setPosition(0, 0);
+
+        bg = new Background()
 
     }
 
@@ -23,6 +35,14 @@ public class Level1State extends GameState{
 
     @Override
     public void draw(Graphics2D g) {
+
+       //valyti
+        g.setColor(Color.WHITE);
+       g.fillRect(0,0, GamePanel.WIDTH,GamePanel.HEIGHT);
+
+        // piesti tilemapa
+
+        tileMap.draw(g);
 
     }
 
